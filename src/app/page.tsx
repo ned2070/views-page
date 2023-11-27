e5014c64fe92cd6aa84574632f3631a637895f70
ned2070te <ned2070te@gmail.com>
2023-11-27 11:36:20 +0000

    postgres set up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
-import image from "next/image";
+import { sql } from "@vercel/postgres";
 
-export default function Home() {
-  return;
-  <main></main>;
+export default async function Home() {
+  await sql`INSERT INTO ViewsTable (views) SELECT 0 WHERE NOT EXISTS (SELECT * FROM ViewsTable)`;
+  await sql`UPDATE ViewsTable SET views = views + 1`;
+  const result = await sql`SELECT views from ViewsTable`;
+
+  console.log(result);
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-600 font-mono">
+      <p>You're looking at my root route.</p>
+    </main>
+  );
 }
